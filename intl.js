aa227d93a523f160a7982c29b7663e144d1fab58
Oliver <matthrews@outlook.com>
2022-03-24 19:33:04 +0800

    update

--- intl.js
+++ intl.js
@@ -1,11 +1,27 @@
 var tran = new Translater();
 
+var isEnglish = function (path) {
+  return /lan=en/.test(path);
+};
+
+var setLangByURL = function (path) {
+  tran.setLang(path && isEnglish(path) ? "en" : "default");
+};
+
 window.addEventListener("load", () => {
-  tran.setLang(tran.lang_name);
+  setLangByURL(location.search);
 });
 
 window.addEventListener("hashchange", () => {
-  tran.setLang(tran.lang_name);
+  setLangByURL(location.hash);
 });
 
-tran.setLang("default")
+// 卸载监听
+window.onunload = function () {
+  window.removeEventListener("load", () => {
+    setLangByURL(location.search);
+  });
+  window.removeEventListener("hashchange", () => {
+    setLangByURL(location.hash);
+  });
+};
